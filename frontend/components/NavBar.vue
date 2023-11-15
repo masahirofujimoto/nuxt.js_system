b60533d5931b1297b09910434a9748e1b06f557e
masahirofujimoto <hijiri8masa@gmail.com>
2023-11-15 11:48:04 +0900

    vueのUIサンプルの作成

---
+++ frontend/components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav class="bg-gray-800 text-white">
+    <ul class="flex justify-center space-x-4 py-4">
+      <li><nuxt-link to="/" class="hover:text-gray-300">ホーム</nuxt-link></li>
+      <li><nuxt-link to="/genre" class="hover:text-gray-300">ジャンル</nuxt-link></li>
+      <li><nuxt-link to="/profile" class="hover:text-gray-300">プロフィール</nuxt-link></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  // コンポーネントの設定
+}
+</script>
+
+<style>
+/* ここにナビゲーションバーのスタイルを追加 */
+</style>
---
+++ frontend/pages/genre.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <NavBar/>
+        <h1>ジャンル検索</h1>
+        <p>ここには各ジャンルの検索ができるようなページにしたいと思います</p>
+    </div>
+</template>
+
+<script>
+import NavBar from '~/components/NavBar.vue'
+
+export default {
+  components: {
+    NavBar
+  }
+}
+</script>
